1dd625d54d2eee1868daecf80eee94bbe26719d4
micheale2021 <andemariammicheale@gmail.com>
2023-01-31 10:46:23 +0000

    complete exercise 2

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,16 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let numRegEx = /[0-9]/g;
+   if(numbers === numRegEx){
+    const average = numbers.reduce((a, b) => a + b) / numbers.length;
+    return average;
+   }
 
 module.exports = average;
+
+
+
+
+// return numbers from an array of numbers and strings
