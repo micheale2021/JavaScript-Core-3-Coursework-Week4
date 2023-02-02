563162da64006d4899acf6464670b01aa310356f
micheale2021 <andemariammicheale@gmail.com>
2023-02-02 10:44:47 +0000

    update get average exercise

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -3,15 +3,16 @@
 // be sure to exclude the strings
 
 function average(numbers) {
-    let numRegEx = /[0-9]/g;
-   if(numbers === numRegEx){
-    const average = numbers.reduce((a, b) => a + b) / numbers.length;
-    return average;
-   }
+  //     let numRegEx = /[0-9]/g;
+  //    if(numbers === numRegEx){
+  //     const average = numbers.reduce((a, b) => a + b) / numbers.length;
+  //     return average;
+
+  let numRegEx = numbers.filter((value) => typeof value === "number");
+  let average = numRegEx.reduce((acc, n) => acc + n);
+  return average / numRegEx.length;
+}
 
 module.exports = average;
-
-
-
 
 // return numbers from an array of numbers and strings
